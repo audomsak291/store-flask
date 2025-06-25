a4e3b95c43afaec1bd351daa6f0f229db69d6fc5
audomsak291 <avn_303@hotmail.com>
2025-06-25 10:41:56 +0700

    fix: remove app.run() for Render compatibility

--- app.py
+++ app.py
@@ -131,6 +131,6 @@ def history():
     conn.close()
     return render_template('history.html', transactions=rows)
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+#if __name__ == '__main__':
+#    port = int(os.environ.get("PORT", 10000))
+#    app.run(host='0.0.0.0', port=port, debug=True)
